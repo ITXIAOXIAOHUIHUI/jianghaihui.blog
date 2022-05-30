fe11818d0902c6f8072bbbbb9906439f85edc5c9
jianghaihui <jianghaihui@flashhold.com>
2022-05-30 10:52:47 +0800

    init

---
+++ src/main/java/com/jianghaihui/levi/jianghaihui_blog/JianghaihuiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jianghaihui.levi.jianghaihui_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianghaihuiBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianghaihuiBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jianghaihui/levi/jianghaihui_blog/JianghaihuiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianghaihui.levi.jianghaihui_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JianghaihuiBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
